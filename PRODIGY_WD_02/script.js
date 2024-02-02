552383ba765dd3377c7d50265836aaa7b042a976
Nihar Arora <86546648+nihararor@users.noreply.github.com>
2024-02-02 21:03:53 +0530

    Update script.js

--- PRODIGY_WD_02/script.js
+++ PRODIGY_WD_02/script.js
@@ -1,16 +1,12 @@
 var ms = 0, s = 0, m = 0, h = 0
 var timer;
-
 var display = document.querySelector(".timer-display")
 var laps = document.querySelector(".laps")
-
-
 function start(){
     if(!timer){
         timer = setInterval(run, 10)
     }
 }
-
 function run(){
     display.innerHTML = getTimer()
     ms++              
@@ -31,25 +27,16 @@ function run(){
         h = 1
     } 
 }
-
 function getTimer(){
     return (h<10 ? "0" + h: h) + " : " + (m<10 ? "0" + m : m) + " : " + (s<10 ? "0" + s : s) + " : " + (ms<10 ? "0" + ms : ms);
 }
-
-
-
 function pause(){
     stopTimer()  
 }
-
 function stopTimer(){
     clearInterval(timer)
     timer = false 
 }
-
-
-
-
 function reset(){
     stopTimer()
     ms = 0
@@ -58,19 +45,12 @@ function reset(){
     h = 0
     display.innerHTML = getTimer()
 }
-
-
-
-
 function restart(){
     if(timer){ 
         reset()
         start()
-    }
-    
+    }    
 }
-
-
 // lap = taking screenshot of current time...
 function lap() {
     if(timer) {   
@@ -79,7 +59,6 @@ function lap() {
         laps.appendChild(Li) 
     }
 }
-
 function resetLap(){
     laps.innerHTML = ""
 }
